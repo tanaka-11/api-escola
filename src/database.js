3a1d154aa4bbcef67f98d008de8e53cbba3cd252
Marina Tanaka <huttenloucher@gmail.com>
2023-09-20 09:51:34 -0300

    exportando a conexao

--- src/database.js
+++ src/database.js
@@ -9,11 +9,14 @@ const conexao = mysql2.createConnection({
     database: 'apiescola'
 });
 
-// Executando a conexão com o comando -> conexao.connect();
+// Executando a conexão com o comando -> conexao.connect(); ele por si só ja é eficiente mas passamos um if/else para termos mensagens mais amigaveis de erro.
 conexao.connect(erro => {
     if (erro) {
         console.error(`Erro de conexão: ${erro.message}`)
     } else {
         console.log(`Database conectaco com sucesso!`);
     }
-})
+});
+
+// Default é utilizado para exportar apenas um arquivo
+export default conexao;
